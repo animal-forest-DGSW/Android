c1ddf8c4b116346dbd8af30ac1eb6d2909ded533
{kdh1123} <81676485+kdh1123@users.noreply.github.com>
2022-07-19 17:43:08 +0900

    Add : ReportFragment
    2022-07-19

---
+++ app/src/main/java/com/example/hackathon/view/fragment/ReportFragment.kt
@@ -0,0 +1,41 @@
+package com.example.hackathon.view.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.hackathon.databinding.FragmentMenuBinding
+
+class ReportFragment : Fragment(){
+    private var _binding: FragmentMenuBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentMenuBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
