8a09e995a08265d20c27db851aca79c7da6a8c3f
{kdh1123} <81676485+kdh1123@users.noreply.github.com>
2022-07-19 16:13:46 +0900

    Add : SignUpActivity 이동 구현
    2022-07-19

--- app/src/main/java/com/example/hackathon/view/activity/SignUpActivity.kt
+++ app/src/main/java/com/example/hackathon/view/activity/SignUpActivity.kt
@@ -1,5 +1,25 @@
 package com.example.hackathon.view.activity
 
-class SignUpActivity {
-    
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.example.hackathon.R
+import com.example.hackathon.databinding.ActivitySignUpBinding
+import com.example.hackathon.viewmodel.SignUpViewModel
+
+class SignUpActivity : AppCompatActivity(){
+    private val binding = ActivitySignUpBinding.inflate(layoutInflater)
+    private val viewModel : SignUpViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_sign_up)
+
+        binding.btnSignup.setOnClickListener {
+            if(viewModel.startLogin())
+                startActivity(Intent(this@SignUpActivity,LoginActivity::class.java))
+        }
+
+    }
 }
--- app/src/main/java/com/example/hackathon/viewmodel/SignUpViewModel.kt
+++ app/src/main/java/com/example/hackathon/viewmodel/SignUpViewModel.kt
@@ -1,7 +1,12 @@
 package com.example.hackathon.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class SignUpViewModel : ViewModel() {
-
+    val id = MutableLiveData<String>()
+    val pw = MutableLiveData<String>()
+    fun startLogin():Boolean{
+        return true
+    }
 }
