a5b96c9e759ebee85969a97bf470c086a1fcdc26
{kdh1123} <81676485+kdh1123@users.noreply.github.com>
2022-07-19 11:59:06 +0900

    Modify : Login Fragment
    2022-07-19

---
+++ app/src/main/java/com/example/hackathon/model/Retrofit.kt
@@ -0,0 +1,4 @@
+package com.example.hackathon.model
+
+class Retrofit {
+}
--- app/src/main/java/com/example/hackathon/view/activity/LoginActivity.kt
+++ app/src/main/java/com/example/hackathon/view/activity/LoginActivity.kt
@@ -1,7 +1,28 @@
 package com.example.hackathon.view.activity
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.hackathon.R
+import com.example.hackathon.databinding.ActivityLoginBinding
+import com.example.hackathon.view.fragment.LoginFragment
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
 
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        val fragment = LoginFragment()
+        fragmentTransaction.add(R.id.fv_login,fragment)
+        fragmentTransaction.commit()
+    }
 }
